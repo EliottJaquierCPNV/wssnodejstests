0e9b0d88d03061df682b6947fbdc87ff07718db5
eliott <eliott.jaquier@gmail.com>
2022-10-23 13:33:27 +0200

    Heroku config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 7071 });
+const port = process.env.PORT || 3000;
+const wss = new WebSocket.Server({ port: port });
 
 const clients = new Map();
 
