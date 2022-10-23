6643eaea01513c89c3141f9b08c9dbd49fb57656
eliott <eliott.jaquier@gmail.com>
2022-10-23 21:20:45 +0200

    Added vercel config and readme

--- client.js
+++ client.js
@@ -1,14 +1,21 @@
 //Create a simple websocket client
 const WebSocket = require('ws');
 
-const ws = new WebSocket('wss://live-node-websockets.herokuapp.com/');
+/**
+ * https://vercel.com/eliottjaquiercpnv
+ */
+
+
+const ws = new WebSocket('wss://wss-nodejs.vercel.app/');
+
+//const ws = new WebSocket('wss://live-node-websockets.herokuapp.com/');
 //Max 100 clients @ 2.8 msg/s (350ms) = 280 msg/s
 //Max 50 clients @ 10 msg/s = 500 msg/s
 //Max 40 clients @ 20 msg/s = 800 msg/s
 
 //40 * 40ms (25 HZ) = 1000 requests / second
 //50 * 60ms (16,6 HZ) = 830 requests / second
-//100 * 345ms (2,9 HZ) = 290 requests / second
+//100 * 345ms (2,9 HZ) = 290 requests / second (100 clients send 2.9 msg/s and server will broadcast it to all clients)
 
 //const ws = new WebSocket('ws://localhost:3000'); 
 //Max 100 clients @ 5 msg/s = 500 msg/s
@@ -18,12 +25,14 @@ const ws = new WebSocket('wss://live-node-websockets.herokuapp.com/');
 
 //If set to 0, it will resend a ping after the server has not responded to the previous ping
 const contactServerEveryMillis = 30;
+const enableSending = false;
 
 const uuid = uuidv4();
 var lastTimePing = Date.now();
 
 ws.on('open', function open() {
     console.log("Server opened!");
+    if(!enableSending) return;
     if(contactServerEveryMillis === 0){
         sendNewPing();
     }else{
---
+++ readme.md
@@ -0,0 +1,24 @@
+Only for me : 
+## Vercel config (impossible)
+https://vercel.com/eliottjaquiercpnv
+https://wss-nodejs.vercel.app/
+
+## Heroku config (deprecated)
+https://dashboard.heroku.com/apps/live-node-websockets
+https://live-node-websockets.herokuapp.com/
+
+## WSS Tester
+https://www.piesocket.com/websocket-tester
+
+```console
+node client.js
+```
+
+## Other installations
+```console
+npm install
+```
+Run
+```console
+npm start
+```
